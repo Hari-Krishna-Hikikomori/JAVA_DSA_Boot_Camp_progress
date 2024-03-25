7dbbba32ea22758b43ee1cef3a450d9f0a4e089e
Krishna <163499327+Hari-Krishna-Hikikomori@users.noreply.github.com>
2024-03-25 21:14:02 +0530

    Create bigDecimal.java

---
+++ Java-Core-concept/bigInteger_and_bigDecimal/bigDecimal.java
@@ -0,0 +1,12 @@
+package bigInteger_and_bigDecimal;
+
+import java.math.BigDecimal;
+
+public class bigDecimal
+{
+    public static void main(String[] args)
+    {
+        BigDecimal BD = new BigDecimal(0.04);
+    }
+
+}
