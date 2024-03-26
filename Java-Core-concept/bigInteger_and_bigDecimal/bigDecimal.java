89b041a1770aa343314a735454fef0cf4ad7aa6a
Krishna <163499327+Hari-Krishna-Hikikomori@users.noreply.github.com>
2024-03-26 23:39:44 +0530

    Bigdicimal
    Signed-off-by: Krishna <163499327+Hari-Krishna-Hikikomori@users.noreply.github.com>

--- Java-Core-concept/bigInteger_and_bigDecimal/bigDecimal.java
+++ Java-Core-concept/bigInteger_and_bigDecimal/bigDecimal.java
@@ -6,7 +6,19 @@ public class bigDecimal
 {
     public static void main(String[] args)
     {
-        BigDecimal BD = new BigDecimal(0.04);
+        BigDecimal A = new BigDecimal("0.040000");
+
+        // error in decimal
+        double a = 0.04;
+        double b = 0.03;
+        // actual value 0.01000000;
+
+        System.out.println(a-b);
+
+        // to get accurate value in small and large number in decimal
+
+        BigDecimal B = new BigDecimal("0.030000");
+        System.out.println(A.subtract(B));
     }
 
 }
