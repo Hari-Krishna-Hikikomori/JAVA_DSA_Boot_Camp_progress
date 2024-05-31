17b5eecac6195ebb454cc693b4856056871ead05
Krishna <163499327+Hari-Krishna-Hikikomori@users.noreply.github.com>
2024-05-31 21:26:37 +0530

    BinaryTree Q & A DFS
    Signed-off-by: Krishna <163499327+Hari-Krishna-Hikikomori@users.noreply.github.com>

---
+++ Java-Core-concept/Trees/BinaryTrees_Problem/DFS/kth_smallestNumber.java
@@ -0,0 +1,54 @@
+package Trees.BinaryTrees_Problem.DFS;
+import Trees.BinaryTrees_Problem.BFS.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class kth_smallestNumber
+{
+
+    public static void main(String[] args)
+    {
+        kth_smallestNumber.Solution test =  new kth_smallestNumber.Solution();
+
+        TreeNode node = new TreeNode(5);
+
+        node.right = new TreeNode(8);
+        node.left  = new TreeNode(3);
+        node.left.left = new TreeNode(1);
+        node.left.left.right = new TreeNode(2);
+        node.right.right = new TreeNode(9);
+
+        System.out.println(test.kthSmallest(node,4));
+
+
+    }
+    static class Solution {
+        public int kthSmallest(TreeNode root, int k)
+        {
+            if(root == null)
+            {
+                return 0;
+            }
+
+            List<Integer> list = new ArrayList<>();
+            return kthSmallest(root,k,list);
+        }
+
+        private int kthSmallest(TreeNode root, int k, List<Integer> ans)
+        {
+            if(root == null) return k;
+
+
+
+            k = kthSmallest(root.left,k,ans)-1;
+            if(k == 0)
+            {
+                ans.add(root.val);
+                return ans.getFirst();
+            }
+
+            return !ans.isEmpty() ? ans.getFirst() : kthSmallest(root.right,k,ans);
+        }
+    }
+}
